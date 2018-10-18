05a36372d8d6d51b8930f26d714f222e997c6320
Paul Clark <clark.paul123@gmail.com>
2018-10-18 17:02:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,9 +38,8 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for number_of_times.each do
+  for number_of_times do
     puts phrase
   end
-end
 end
 
